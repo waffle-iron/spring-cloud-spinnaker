55aa8ede6951487643eeb0e62802e0e349590688
Greg Turnquist <gturnquist@pivotal.io>
2016-05-19 21:48:58 -0500

    Refine styling of UI

--- src/main/js/Settings.js
+++ src/main/js/Settings.js
@@ -18,30 +18,31 @@ class Settings extends React.Component {
 	render() {
 		let labelLayout = 'layout__item u-1/2-lap-and-up u-1/4-desk'
 		let inputLayout = 'layout__item u-1/2-lap-and-up u-1/2-desk'
+		let lineItemLayout = 'control-group'
 		return (
 			<div>
 				<ul className="layout">
-					<li>
+					<li className={lineItemLayout}>
 						<label className={labelLayout}>Redis Service</label>
 						<input className={inputLayout} type="text" name="spring.cloud.deployer.cloudfoundry.defaults.services" onChange={this.handleChange} />
 					</li>
-					<li>
+					<li className={lineItemLayout}>
 						<label className={labelLayout}>Account Name</label>
 						<input className={inputLayout} type="text" name="cf.account.name" onChange={this.handleChange} />
 					</li>
-					<li>
+					<li className={lineItemLayout}>
 						<label className={labelLayout}>Account Password</label>
 						<input className={inputLayout} type="password" name="cf.account.password" onChange={this.handleChange} />
 					</li>
-					<li>
+					<li className={lineItemLayout}>
 						<label className={labelLayout}>Repository Name/Access Code</label>
 						<input className={inputLayout} type="text" name="cf.repo.username" onChange={this.handleChange} />
 					</li>
-					<li>
+					<li className={lineItemLayout}>
 						<label className={labelLayout}>Repository Password/Secret Code</label>
 						<input className={inputLayout} type="text" name="cf.repo.password" onChange={this.handleChange} />
 					</li>
-					<li>
+					<li className={lineItemLayout}>
 						<label className={labelLayout}>Spring Config Location</label>
 						<input className={inputLayout} type="text" name="spring.config.location" onChange={this.handleChange} />
 					</li>
